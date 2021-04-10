8f1cdb67f2b4b9255fedf934a2cea7b86e828b0d
İbrahim Doğan <ibrahimmdogann@gmail.com>
2021-04-10 18:27:38 +0300

    [refactor] remove matrix in flask.yml

--- tests/test_reverse_string.py
+++ tests/test_reverse_string.py
@@ -9,4 +9,4 @@ def test_reverse_string_incorrect(app, client):
     res = client.get('/myservice?word=hello')
     assert res.status_code == 200
     expected = "hello"
-    assert expected == res.get_data(as_text=True)
+    assert expected != res.get_data(as_text=True)
